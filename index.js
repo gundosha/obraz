817faabb9ea287c6aaf140d79c040faf5ebec98b
gundosha <zamilov_2016@mail.ru>
2022-12-14 22:36:43 +0300

    da

--- index.js
+++ index.js
@@ -33,65 +33,6 @@ pricebtnslider.forEach(function(item, i, btnslider){
 
 
 
-
-
-       /*  document.addEventListener('DOMContentLoaded', function(){
-            const form = document.getElementById('form')
-            form.addEventListener('submit', formSend);
-            async function formSend(e){
-                e.preventDefault();
-
-                let error = formValidate(form)
-                let formData = new FormData(form); 
-
-                if (error === 0){
-                    console.log('zalupa')
-                    let response = await fetch('./sendmail.php', {
-                        method: 'POST',
-                        body: formData
-                    });
-                    console.log('zalupa2')
-                    if (response.ok){
-                        let result = await response.json();
-                        alert(result.message);
-                        form.reset();
-                    }else{
-
-                    }
-                }else{
-                    alert('заполните обязательные поля')
-                }
-            }
-
-            function formValidate(form){
-                let error = 0;
-                let formReq = document.querySelectorAll('._req');
-
-                for (let index = 0; index < formReq.length; index++){
-                    const input = formReq[index];
-                    formRemoveError(input);
-
-                    if (input.value === ''){
-                        formAddError(input);
-                        error++
-                    }
-                }
-                return error;
-            }
-
-        function formAddError(input){
-            input.parentElement.classList.add('_error');
-            input.classList.add('_error')
-        }
-
-        function formRemoveError(input){
-            input.parentElement.classList.remove('_error')
-            input.classList.remove('_error')
-        }
-        }) */
-
-
-
 function open_sublist(){
     open_menu.classList.toggle('header__menu_burger_block')
 }
