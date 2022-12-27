9a75fb43919b73b29b4467fad711af690ab1c1c6
gundosha <zamilov_2016@mail.ru>
2022-12-27 23:11:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const sliderblock = document.querySelector('.clients__container_com')
 
 btnslider.forEach(function(item, i, btnslider){
     item.addEventListener('click', function(){
-        offset -=380
-        if (offset < -760){
+        offset -=400
+        if (offset < -800){
             offset = 0
         }
         sliderblock.style.left = offset + 'px'
